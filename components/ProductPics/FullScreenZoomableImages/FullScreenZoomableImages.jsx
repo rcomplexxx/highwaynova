2344d009bd1c3293502c635ded19b3a7d22336c9
rcomplexxx <rcomplexx@gmail.com>
2024-04-06 17:41:11 +0200

    :testing bug fix

--- components/ProductPics/FullScreenZoomableImages/FullScreenZoomableImages.jsx
+++ components/ProductPics/FullScreenZoomableImages/FullScreenZoomableImages.jsx
@@ -175,8 +175,8 @@ const FullScreenZoomableImage = ({
     const fixedZoomDiv = fixedZoomDivRef.current;
     const bgColor = getComputedStyle(document.documentElement).getPropertyValue('--bg-color');
    
-    const baseRgb=`${parseInt(bgColor.slice(1, 3), 16)}, ${parseInt(bgColor.slice(3, 5), 16)}, ${parseInt(bgColor.slice(5, 7), 16)},`
-    const getRgbValues = (opacity) =>{ return `rgba(${baseRgb} ${opacity})`};
+    const baseRgb=`${parseInt(bgColor.slice(1, 3), 16)}, ${parseInt(bgColor.slice(3, 5), 16)}, ${parseInt(bgColor.slice(5, 7), 16)}`
+    const getRgbValues = (opacity) =>{ return `rgba(${baseRgb}, ${opacity})`};
 
 
     let timeoutId;
@@ -219,8 +219,7 @@ const FullScreenZoomableImage = ({
       if (swipeYLock || zoomed || multiTouchDetected) return;
       if (event.touches.length > 1) {
         multiTouchDetected=true;
-        imgDiv.style.transition =
-        "transform 0.3s ease, background-color 0.3s ease";
+        imgDiv.style.transition = "transform 0.3s ease, background-color 0.3s ease";
       imgDiv.style.transform = `translateY(${0}px)`;
 
       fixedZoomDiv.style.backgroundColor = getRgbValues(1);
