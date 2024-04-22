a082f6b40dd545adc7b9003626f3c6d40bf906da
rcomplexxx <rcomplexx@gmail.com>
2024-04-22 23:47:49 +0200

    :design improvements

--- components/ProductPics/FullScreenZoomableImages/ToastMessage/ToastMessage.jsx
+++ components/ProductPics/FullScreenZoomableImages/ToastMessage/ToastMessage.jsx
@@ -13,58 +13,6 @@ export default function ToastMessage({showToastMessage, setShowToastMessage}) {
         const toast = toastRef.current;
         
 
-       
-        if(showToastMessage==1){
-       
-
-        toastTimeout.current= setTimeout(()=> {
-           
-            if(toast){
-            
-            toast.classList.add(styles.dissapearingToast);      
-      
-            setTimeout(()=>{
-              global.toastMessageNotShowable=true;
-              setShowToastMessage(0);
-              
-            },500);
-            
-            
-          }
-          },
-          4500);
-        }
-
-
-
-
-        else if(showToastMessage==2){
-
-            clearTimeout(toastTimeout.current);
-          
-               
-                if(toast){
-                    toast.classList.add(styles.dissapearingToast); 
-
-                 
-                setTimeout(()=>{  global.toastMessageNotShowable=true;setShowToastMessage(0)},300)    
-                } 
-            
-        }
-
-        else if(showToastMessage==3){
-            clearTimeout(toastTimeout.current);
-          
-              if(toast){
-                toast.classList.add(styles.instantDissapearingToast);
-             
-                global.toastMessageNotShowable=true;
-                setTimeout(()=>{  global.toastMessageNotShowable=true;setShowToastMessage(0)},150)    
-              }
-        
-        
-            
-        }
 
     },[showToastMessage, toastRef])
 
