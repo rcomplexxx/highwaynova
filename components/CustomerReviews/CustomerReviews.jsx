49ffe248f326b7a2aed690c80a59661a196c155f
rcomplexxx <rcomplexx@gmail.com>
2024-05-24 14:03:14 +0200

    :optimized

--- components/CustomerReviews/CustomerReviews.jsx
+++ components/CustomerReviews/CustomerReviews.jsx
@@ -2,7 +2,6 @@ import React, { useState, useRef, useCallback, useEffect } from "react";
 import Image from "next/image";
 import styles from "./customerreviews.module.css";
 import StarRatings from "react-star-ratings";
-import ReactHtmlParser from "react-html-parser";
 
 import WriteReviewVisible from "./WriteReview/WriteReviewVisible";
 import FullScreenReview from "./FullScreenReview/FullScreenReview";
@@ -49,7 +48,7 @@ function Review({ product_id,  name, text,  stars, imageNames, setFullScreenRevi
           starSpacing="2px"
         />
     </div>
-      <p className={styles.reviewText}>{ReactHtmlParser(text)}</p>
+      <p className={styles.reviewText} dangerouslySetInnerHTML={{ __html:text}}></p>
       <p className={styles.reviewAuthor}>{name}</p>
     </div>
   );
