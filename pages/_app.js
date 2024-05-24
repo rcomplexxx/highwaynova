0433c28dc1b8890908a5a6db32f9a9c5136cd409
rcomplexxx <rcomplexx@gmail.com>
2024-05-24 19:19:22 +0200

    :email popup improved

--- pages/_app.js
+++ pages/_app.js
@@ -173,7 +173,7 @@ const totalItems= useMemo(()=>{
         <DefaultSeo {...SEO}/>
        
       
-      {<EmailFlowPopup setEmailPopup={setEmailPopup}/>}
+      {emailPopup && <EmailFlowPopup setEmailPopup={setEmailPopup}/>}
     {!router.pathname.includes('admin') && <Navbar totalItems={totalItems}  newProduct={newProduct} setNewProduct={setNewProduct}/>}
 
       
