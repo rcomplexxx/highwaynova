ba046241a4bc9948660878fe8df7e2656a2fc1b3
rcomplexxx <rcomplexx@gmail.com>
2024-06-13 14:58:10 +0200

    :optimized

--- components/Cards/PolicyCard/PolicyCard.jsx
+++ components/Cards/PolicyCard/PolicyCard.jsx
@@ -7,14 +7,14 @@ import styles from "./policyCard.module.css";
 
 export default function PolicyCard(props) {
   return (
-    <div
-      className={styles.policy_background_div}
-    >
+  
+    
       <div
       className={styles.policy_main_div}
     >
         {props.children}
         </div>
-    </div>
+ 
+ 
   );
 }
