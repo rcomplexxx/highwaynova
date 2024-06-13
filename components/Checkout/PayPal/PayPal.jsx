134b00272004a69cd7ff08e07daae2747ef6805b
rcomplexxx <rcomplexx@gmail.com>
2024-06-13 16:26:25 +0200

    :optimized

--- components/Checkout/PayPal/PayPal.jsx
+++ components/Checkout/PayPal/PayPal.jsx
@@ -148,14 +148,12 @@ const PayPalButton=({checkFields, organizeUserData, method='paypal',  type='norm
 
       const cancelHandler = async () => { setPaypalError('Payment is canceled.') };
       
-    
-      
+
 
     return  <PayPalScriptProvider
     options={{
       "client-id":
         "AQB3vOguzerJ-HXgJavEAMlivjs3DTNyWi2W7yKI94arI23zXOAaSJx4Zf4JzTO9RjvJdr5AflrFHWp1",
-        components: 'buttons'
     }}
   >
       <PayPalButtons
