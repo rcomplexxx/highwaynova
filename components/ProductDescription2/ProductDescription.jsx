e0f66c7f6cf9688724f5c83dfbdd9d9eb9b75282
rcomplexxx <rcomplexx@gmail.com>
2024-06-14 21:35:58 +0200

    :testing

--- components/ProductDescription2/ProductDescription.jsx
+++ components/ProductDescription2/ProductDescription.jsx
@@ -1,9 +1,8 @@
-
-
+import  { useLayoutEffect, useState } from 'react'
 
 import styles from './productdescription.module.css'
 
-import parse from 'html-react-parser';
+import ReactHtmlParser from "react-html-parser";
 import Image from 'next/image';
 
 export default function ProductDescription({description}) {
@@ -11,30 +10,31 @@ export default function ProductDescription({description}) {
   
 
 
-  // const options = {
-  //   transform: (node) => {
-  //     // Replace <img> tags with Next.js <Image> components
-  //     if (node.type === 'tag' && node.name === 'img' && node.attribs) {
-  //       return (
-  //         <Image
-  //           src={node.attribs.src}
-  //           alt={node.attribs.alt || ''}
-  //           className={node.attribs.class || ''}
-  //           layout='responsive'
-  //           width={0} // set your desired width
-  //           height={0} // set your desired height
-  //           sizes="(max-width: 980px) 10px, 100vw"
-  //         />
-  //       );
-  //     }
-  //   }
-  // };
+  const options = {
+    transform: (node) => {
+      // Replace <img> tags with Next.js <Image> components
+      if (node.name === 'img') {
+        return (
+          <Image
+            src={node.attribs.src}
+            alt={node.attribs.alt}
+            className={node.attribs.class}
+           
+            width={0} // set your desired width
+            height={0} // set your desired height
+            sizes="(max-width: 600px) 100vw, 512px"
+          />
+        );
+      }
+    }
+  };
 
-  //, options
+ 
+
 
   
-
-
+  // import parse from 'html-react-parser';
+  // import Image from 'next/image';
   // const replaceImgWithNextImage = () => {
   //   return parse(description, {
   //     replace: (domNode) => {
@@ -54,13 +54,6 @@ export default function ProductDescription({description}) {
     <div className={styles.descriptionDiv}>
       <div className={styles.mainSpan}>Product details</div>
 
-      {parse(description, {
-      replace: (domNode) => {
-        if (domNode.name === 'img') {
-          const { src, alt, class: className } = domNode.attribs;
-          return <Image src={src} alt={alt} className={className} width={0} height={0} sizes="(max-width: 600px) 100vw, 512px" />;
-        }
-      },
-    })}</div>
+      {ReactHtmlParser(description, options)}</div>
   )
 }
