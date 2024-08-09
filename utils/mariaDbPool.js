92bf88fc39310e4e160cb052ded50033463b01b1
rcomplexxx <rcomplexx@gmail.com>
2024-08-09 02:13:59 +0200

    :testing db

--- utils/mariaDbPool.js
+++ utils/mariaDbPool.js
@@ -7,14 +7,27 @@ function getPool() {
     if (!pool) {
     
 
+        // pool = createPool({
+        //     host: process.env.DB_HOST,
+        //     user: process.env.DB_USER,
+        //     password: process.env.DB_PASSWORD,
+        //     database: process.env.DB_DATABASE,
+        //     port: process.env.DB_PORT,
+        //     connectionLimit: 10,
+        // });
+
+
         pool = createPool({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
+            host: sql7.freesqldatabase.com,
+            user: sql7724818,
+            password: nxPnr5hUPr,
+            database: sql7724818,
+            port: 3306,
             connectionLimit: 10,
         });
     }
+
+    
     return pool;
 }
 
