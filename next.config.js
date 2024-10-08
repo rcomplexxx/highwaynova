9b3a8580224fcd7312ce93acbf7794bc56d87e9e
rcomplexxx <rcomplexx@gmail.com>
2024-10-08 05:34:43 +0200

    :bug fixed

--- next.config.js
+++ next.config.js
@@ -17,11 +17,7 @@ const nextConfig = {
   },
    
 
-    experimental: {
-        scrollRestoration: true,
-    
-     
-      },
+  scrollRestoration: true,
       images: {
         deviceSizes: [320, 360, 400, 440, 480, 520, 560, 600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       },
