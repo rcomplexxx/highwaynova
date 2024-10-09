9d200d57de201f5611b67864a4d6292fe0eb4574
rcomplexxx <rcomplexx@gmail.com>
2024-10-09 14:13:57 +0200

    :optimized

--- components/ProductPage/ProductPics/ProductPics.jsx
+++ components/ProductPage/ProductPics/ProductPics.jsx
@@ -184,7 +184,9 @@ useLayoutEffect(()=>{
         preventClicks={false}
         // preventClicksPropagation={false}
         touchStartPreventDefault={false}
-      
+        preloadImages={false}
+        lazy={true}
+        
         
 
         >
