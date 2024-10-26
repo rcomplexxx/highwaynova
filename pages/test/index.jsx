cc47b94c0ba1a97dd4fb40ca2dc9339bbcaae922
rcomplexxx <rcomplexx@gmail.com>
2024-10-27 01:13:47 +0200

    :testing

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -3,8 +3,8 @@ import styles from './teststyles.module.css'
 
 export default function index() {
   return (
-    <div className={styles.mainDiv}>{Array.from({ length: 100000 }, (_, index) => (
-        <div key={index + 1}>
+    <div>{Array.from({ length: 10000 }, (_, index) => (
+        <div className={styles.myDiv} key={index + 1}>
           Item {index + 1}
         </div>
       ))}</div>
